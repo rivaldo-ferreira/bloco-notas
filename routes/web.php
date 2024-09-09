1834bb630b05e0b476ea18ad5b7f5449a52301c0
Rivas <rivaldo@gmail.com>
2024-09-09 09:21:20 -0300

    008

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
