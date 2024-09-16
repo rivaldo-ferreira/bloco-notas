d7ba5ecdbab662073d01531c40efe66aaf4cabc0
Rivas <rivaldo@gmail.com>
2024-09-16 13:28:40 -0300

    021

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,9 +10,13 @@ class MainController extends Controller
     public function index()
     {
         // show users
+        $id = session('user.id');
+        // $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
 
         //home
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
