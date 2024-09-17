a291f66384dbf07f2c4b7976012de827ee88bd13
Rivas <rivaldo@gmail.com>
2024-09-17 20:12:07 -0300

    031 - criando o services

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -29,23 +30,13 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decrypt($id);
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decrypt($id);
-    }
-
-    //function decrypt
-    private function decrypt($id)
-    {
-        //check decrypt id
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-        return $id;
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
     }
 }
